8c8e0488525abe80c293c391a34b57fdd6da216b
eckyecky <765268+eckyecky@users.noreply.github.com>
2020-11-28 12:21:41 +1100

    Update docker configuration

--- docker/rootfs/entrypoint.sh
+++ docker/rootfs/entrypoint.sh
@@ -1,5 +1,26 @@
-#!/usr/bin/env sh
+#!/bin/sh
+
+file_permissions() {
+  local file=$1 user=$3 perms=$4
+  current="$(stat -c "%U %G %04a" "$file")"
+  if [ "$current" != "$user $user $perms" ];then
+    chown "$user:$user" "$file"
+    chmod $perms "$file"
+  fi
+}
+file_permissions /bin/ssh-honeypot true root 0775 0775
+file_permissions /bin/ssh-honeypot-wrapper true root 0775 0775
+file_permissions /etc/services.d/sshd/run true root 0775 0775
+file_permissions /home/honeycomb true honeycomb 2770 2770
+
 set -e
+
+RSA_KEY="/home/honeycomb/rsa/sshd-key.rsa"
+
+if [ ! -f "${RSA_KEY}" ]; then
+  ssh-keygen -t rsa -f "${RSA_KEY}" -q -N ""
+  chown honeycomb:honeycomb "${RSA_KEY}"
+fi
 
 # Initial arguments that ssh-honeypot will be started with
 # Intentionally added one by one for more clarity
